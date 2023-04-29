3bcd250df5aac24ca1cfa172826550d5a665987e
Handayani Meiatara <hmeiatara@gmail.com>
2023-04-29 20:53:47 +0700

    Menambahkan Homepage awal

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+/**
+ * navbar
+ */
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navLinks = document.querySelectorAll("[data-navbar-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    header.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+/**
+ * Form
+ */
+
+function handleGetFormData() {
+  const name = document.getElementById('name').value;
+  const city = document.getElementById('city').value;
+  const email = document.getElementById('email').value;
+  const zipCode = document.getElementById('zip-code').value;
+  const status = document.getElementById('status').checked;
+  
+  return {
+    name,
+    city,
+    email,
+    zipCode,
+    status
+  };
+}
+
+function isNumber(string) {
+  return !isNaN(string);
+}
+
+function checkboxIsChecked() {
+  return document.getElementById('status').checked;
+}
+
+function validateFormData(formData) {
+  if (
+    formData &&
+    isNumber(formData.zipCode) &&
+    checkboxIsChecked()
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function submit(event) {
+  const formData = handleGetFormData();
+  if (!validateFormData(formData)) {
+    document.getElementById('warning').innerHTML = 'Periksa form anda sekali lagi';
+  } else {
+    document.getElementById('warning').innerHTML = '';
+    // Do something with the form data
+  }
+}
+
+document.getElementById('submit-form').addEventListener('click', submit);
